5beb36e0f31f24521591e48576d5589df46c44fe
christian leube <christianleube@hotmail.com>
2023-04-11 14:00:13 +0200

    Corrected Grafana link

--- techbuddy/src/pages/DashBoard_Grafana.js
+++ techbuddy/src/pages/DashBoard_Grafana.js
@@ -3,7 +3,7 @@ import './css/DashBoard_Grafana.css'
 const Grafana = () => {
     return (
         <div className="div-grafana">
-            <iframe src="http://localhost:30091/d/BVI_WIf4k/techbuddy-getanswer?orgId=1" width="1500" height="1000"></iframe>
+            <iframe src="http://localhost:30091/d/xC07X_YVz/techbuddy?orgId=1" width="1500" height="1000"></iframe>
         </div>
     )
   };
